808709535325057d525a76f76a85707ae69e18c5
Arman <riasiarman@yahoo.com>
2020-08-20 23:30:41 +0430

    Launch Screen and main storyboard are added.

---
+++ Photo Converter/Photo Converter/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Photo Converter
+//
+//  Created by Arman Riasi on 8/20/20.
+//  Copyright © 2020 Arman Riasi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Main Screen"
+    }
+    
+    
+    
+
+
+
+}
